fb5d30e79100f33f83a752d78d63d94c0b96333e
wilson2403 <wilson2403@gmail.com>
2023-02-24 16:22:40 -0600

    Appvance Commit [skip ci] - [adding testwilson1.js]

--- testwilson1.js
+++ testwilson1.js
@@ -1,4 +1,4 @@
-if ($proxyHost.length > 0 && $proxyPort != 0 && $capturePort != 0) {
+//if ($proxyHost.length > 0 && $proxyPort != 0 && $capturePort != 0) {
 	enableNetwork($proxyHost, $proxyPort, $capturePort);
 }
 setFailOnValidations(true);
